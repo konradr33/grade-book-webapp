466f819661110bfcaca55788b2bb21ea799615bb
Konrad Radoń <konradradon@gmail.com>
2021-05-13 17:44:50 +0200

    Add login page

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserRole } from '../../../mocels/user-role';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent {
+  public loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = fb.group({
+      login: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      role: [UserRole.ADMIN, [Validators.required]],
+    });
+  }
+
+  public onLoginSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
+}
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginModule } from './login/login.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, LoginModule],
+  exports: [LoginModule],
+})
+export class PagesModule {
+}
